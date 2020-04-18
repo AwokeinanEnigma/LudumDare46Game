912748f6f8c0d5bc6cc1af3b1f6dcc29dab964f2
some dude <kurogames7595@gmail.com>
2020-04-18 13:56:14 -0400

    healthComponent implementation

---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Tell unity that we need this component.
+namespace LudumDare46Game
+{
+    [RequireComponent(typeof(StatComponent))]
+
+    public class HealthComponent : MonoBehaviour
+    {
+        private StatComponent statComponent;
+        private float health;
+        void Start()
+        {
+            this.statComponent = gameObject.GetComponent<StatComponent>();
+            this.health = statComponent.baseHealth;
+        }
+        void TakeDamage(DamageInfo damageInfo)
+        {
+            //do stuff
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if (this.health <= 0) { Destroy(gameObject); };
+        }
+    }
+}
---
+++ Assets/Scripts/StatComponent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//component for pretty much everything.
+public class StatComponent : MonoBehaviour
+{
+    public float baseHealth;
+    public float baseArmor;
+    public float a; 
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+        
+    }
+}
