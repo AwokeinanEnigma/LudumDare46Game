3d2dfc5d48a63d12ad7d54ccaa68155921211bde
JudsonEsq <jude@holt-info.com>
2020-04-18 15:32:32 -0400

    Added basic left/right movement and jumping to PlayerController.cs, as of yet untested.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D playerRigidBody;
+
+
+    public Transform groundDetectionTransform;
+    public float groundDetectionSize;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector2 boxDimensions = new Vector2(groundDetectionSize, groundDetectionSize);
+        isTouchingGround = Physics2D.OverlapBox(groundDetectionTransform.position, boxDimensions, groundLayer);
 
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = 0;
+        if (Input.GetAxis("Jump") >= 1 && isTouchingGround)
+        {
+            moveVertical = 30;
+        }
+
+        playerRigidBody.AddForce(new Vector2  (moveHorizontal, moveVertical));
     }
 }
