16c50001332a9536e3d23837595a79278f26aa07
some dude <kurogames7595@gmail.com>
2020-04-19 17:02:29 -0400

    got the main character sprite working but also broke jumping. jud pls fix.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace LudumDare46Game
 {
+    public class PlayerController : MonoBehaviour
+    {
 
-    private Rigidbody2D playerRigidBody;
-    //yeah.
+        private Rigidbody2D playerRigidBody;
+        //yeah.
 
-    public Transform groundDetectionTransform;
-    public float groundDetectionSize;
-    public LayerMask groundLayer;
-    private bool isTouchingGround;
+        public Transform groundDetectionTransform;
+        public float groundDetectionSize;
+        public LayerMask groundLayer;
+        private bool isTouchingGround;
 
-    public float speed;
-    public float jumpHeight;
+        public float speed;
+        public float jumpHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -23,19 +25,19 @@ public class PlayerController : MonoBehaviour
         speed = 10;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Vector2 boxDimensions = new Vector2(groundDetectionSize, groundDetectionSize);
-        isTouchingGround = Physics2D.OverlapBox(groundDetectionTransform.position, boxDimensions, groundLayer);
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = 0;
-        if (Input.GetAxis("Jump") >= 1 && isTouchingGround)
+        // Update is called once per frame
+        void FixedUpdate()
         {
-            moveVertical = jumpHeight;
-        }
+            Vector2 boxDimensions = new Vector2(groundDetectionSize, groundDetectionSize);
+            isTouchingGround = Physics2D.OverlapBox(groundDetectionTransform.position, boxDimensions, groundLayer);
 
-        playerRigidBody.velocity = (new Vector2  (moveHorizontal * speed, playerRigidBody.velocity.y + moveVertical));
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = 0;
+            if (Input.GetAxis("Jump") >= 1 && isTouchingGround)
+            {
+                moveVertical = jumpHeight;
+            }
+            playerRigidBody.velocity = (new Vector2(moveHorizontal * speed, playerRigidBody.velocity.y + moveVertical));
+        }
     }
 }
